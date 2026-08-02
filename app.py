45ea4dc7fda16677e40a371a8f6d7c59a227d840
Bhanu pratap <bhanurajput2409@gmail.com>
2026-08-02 22:58:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # This code is from https://github.com/LondheShubham153/flask-app-ecs/tree/main
-# Flask App ( added )
+# Flask App
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -7,6 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
 
 @app.route('/health')
 def health():
