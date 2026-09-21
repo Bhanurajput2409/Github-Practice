b482ac7276d26c505ce4256f2e302789d56a76c6
Bhanu pratap <bhanurajput2409@gmail.com>
2026-09-22 01:27:04 +0530

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// https://jestjs.io/docs/configuration
+export default () => {
+  return {
+    testEnvironment: 'node',
+    roots: ['test/'],
+
+    // Disables CommonJS transforms (see https://jestjs.io/docs/ecmascript-modules)
+    transform: {},
+
+    setupFilesAfterEnv: ['./testing/jestRedisMock.js'],
+  };
+};
